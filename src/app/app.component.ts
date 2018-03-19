b91473933d779434545f62ca97430df5e2658841
Andrew Leung <andrew@ualberta.net>
2018-03-19 18:18:29 -0400

    FEATURE: add NumberGeneratorService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
+import { NumberGeneratorService } from './number-generator.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+
+  providers: [NumberGeneratorService]
 })
 export class AppComponent {
   title = 'app';
 
+  constructor( private _numberGeneratorService: NumberGeneratorService ) {}
+
   numberGenerator(): number {
     return 42;
+  };
+
+  fromNumberGeneratorService(): number {
+    return this._numberGeneratorService.value();
   };
 
 }
