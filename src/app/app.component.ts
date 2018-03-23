ea16b1eeffda4d52ce1cd9b96f6c27ea91605dfe
Andrew Leung <andrew@ualberta.net>
2018-03-23 18:54:45 -0400

    FEATURE: Add HTTPClientModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NumberGeneratorService } from './number-generator.service';
 
 @Component({
@@ -8,17 +8,26 @@ import { NumberGeneratorService } from './number-generator.service';
 
   providers: [NumberGeneratorService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  valueHTTPFile: number;
 
   constructor( private _numberGeneratorService: NumberGeneratorService ) {}
 
   numberGenerator(): number {
     return 42;
-  };
+  }
 
   fromNumberGeneratorService(): number {
     return this._numberGeneratorService.value();
-  };
+  }
 
+  fromNumberGeneratorServiceHTTPFile() {
+    this._numberGeneratorService.valueHTTPFile()
+      .subscribe(value => this.valueHTTPFile = value.value);
+  }
+
+  ngOnInit(): void {
+    this.fromNumberGeneratorServiceHTTPFile();
+  }
 }
